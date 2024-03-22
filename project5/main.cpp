15b649c026c74e44de1381cc47308c9e1cadbae3
liloozmo <osmo_t1@denison.edu>
2024-03-21 22:17:36 -0400

    parsedone

--- project5/main.cpp
+++ project5/main.cpp
@@ -17,6 +17,12 @@
 
 using namespace std;
 
+//==========================================
+//functiom decleration
+//==========================================
+void countcharacters(const string &filename, vector<int> &charCounts );
+
+
 //==============================================
 // main
 // Run test case on heap class
@@ -26,13 +32,17 @@ using namespace std;
 int main ( void)
 {
 
-    vector<int> store(26);
-    char temp;
+    vector<int> charCounts(26);
+    string filename = "WarPeace.txt";
 
-    if( 'a' <= temp <= 'z'){
-
-        store[cchar- 'a']++;
+    countcharacters(filename, charCounts);
+    for (int i =0; i < 26; i++)
+    {
+        char letter = i +'a';
+        cout << letter << "  " << charCounts[i] << endl;
     }
+
+    
  
     priority_queue<BT<char>> pq;
 
@@ -40,5 +50,33 @@ int main ( void)
 
 
     return 0;
+}
+
+//==============================================
+// countcharacters
+// open and read a text file and count the frequency of each
+// character between a-z in the text file.
+// INPUT: const string &filename, vector<int> &charCounts
+// RETURN: void 
+//==============================================
+void countcharacters(const string &filename, vector<int> &charCounts )
+{
+    ifstream file(filename);
+    if (!file.is_open())
+        throw runtime_error("Error opening file");
+    char c;
+    charCounts.assign(26,0);
+
+    while(file.get(c))
+    {
+        c = tolower(c);
+        if(isalpha(c))
+        {
+            int index = c - 'a';
+            charCounts[index]++;
+        }
+        
+    }
+    file.close() // not neccesarry, we need to decide.
 }
 
