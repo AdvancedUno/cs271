6f9cad0a6a0460f6e8b9cc8a2aa318839e429d77
ritsth <akitirsth@gmail.com>
2024-03-28 17:43:20 -0400

    decode and encode

--- project5/decode.cpp
+++ project5/decode.cpp
@@ -2,7 +2,7 @@
 // Decode.cpp
 // Name: EunHo Lee , Ritika, Tomer  
 // Date: 28 mar 2024
-// This file decodes the encodes files.
+// This file decodes the encoded files/text.
 //===============================
 
 #include <ctime>
@@ -15,21 +15,46 @@
 #include "info.h"
 #include <queue>
 #include <fstream> 
-
+#include <map>
+  
   
 
 using namespace std;
 
+// Node* buildBT();
 
 //==============================================
 // main
-// 
+// Decodes the encoded files/text
 // INPUT: none
 // RETURN: int 
 //==============================================
 int main ( void)
 {
-    
+    map<char, string> code_vector;
+
+    for (char i='a';i <= 'z';i++ ){
+      code_vector[i] = "101";
+    }   
+
+    BT mainBT();
+    for (char i='a';i <= 'z';i++ ){
+      
+      for (char each:code_vector[i]){
+        if (each == '0'){
+          cout << "left" << " " ;
+
+        }
+        if (each == '1'){
+          cout << "right" << " ";
+        }
+
+      }
+      cout << code_vector[i] << endl;
+    }
+
 
     return 0;
 }
+
+÷
